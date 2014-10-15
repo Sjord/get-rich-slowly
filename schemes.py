2afac2d8ab9d2da62794ad927f528d4c27561b0d
Sjoerd <sjoerd-github@linuxonly.nl>
2014-10-15 15:25:40 +0200

    Extract loop through prices
    Add a update() method to the scheme, to pass fund prices to. This makes
    it possible to get an advice for each price.

--- schemes.py
+++ schemes.py
@@ -1,6 +1,7 @@
 from __future__ import division
 import sys
 from models import Fund
+import datetime
 
 
 class Advice(object):
@@ -26,25 +27,45 @@ class Ema(object):
     shortDays = 9
     longDays = 14
 
-    def advise(self, fund):
-        emaShort = fund.prices[0].price
-        emaLong = fund.prices[0].price
+    def __init__(self, fund):
+        self.emaShort = fund.prices[0].price
+        self.emaLong = fund.prices[0].price
 
-        for price in [p.price for p in fund.prices]:
-            alpha = 2/(self.shortDays + 1)
-            emaShort = alpha * price + (1 - alpha) * emaShort
+    def update(self, priceobj):
+        price = priceobj.price
+        alpha = 2/(self.shortDays + 1)
+        self.emaShort = alpha * price + (1 - alpha) * self.emaShort
 
-            alpha = 2/(self.longDays+1)
-            emaLong = alpha * price + (1 - alpha) * emaLong
+        alpha = 2/(self.longDays+1)
+        self.emaLong = alpha * price + (1 - alpha) * self.emaLong
 
-        if (emaShort > emaLong):
+        if (self.emaShort > self.emaLong):
             return Advice.buy
-        if (emaLong > emaShort):
+        if (self.emaLong > self.emaShort):
             return Advice.sell
         return Advice.none
 
 
 if __name__ == "__main__":
     isin = sys.argv[1]
+    day = datetime.date.today() - datetime.timedelta(days=365)
+    profit = 0
+    bought = None
+
     fund = Fund.load(isin)
-    print Ema().advise(fund)
+    advisor = Ema(fund)
+    [advisor.update(p) for p in fund.prices[0:-365]]
+
+    for i in range(0, 365):
+        p = fund.prices[i-366]
+        advice = advisor.update(p)
+
+        if advice == Advice.buy and not bought:
+            bought = p.price
+        if advice == Advice.sell and bought:
+            profit += p.price - bought
+            bought = None
+    if bought:
+        profit += fund.prices[-1].price - bought
+    print profit
+
