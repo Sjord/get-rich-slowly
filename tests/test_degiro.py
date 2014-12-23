6cdf6a2c05de6446e8d0d62d53a428ba3e6cc319
Sjoerd <sjoerd-github@linuxonly.nl>
2014-12-23 14:29:29 +0100

    Add Interface class in front of the Session class

--- tests/test_degiro.py
+++ tests/test_degiro.py
@@ -1,11 +1,13 @@
 from unittest2 import TestCase
-from degiro import DeGiroDict
+from degiro import DeGiroDict, Interface
+from mock import Mock
 import json
 
 
 def load_fixture(name):
     with open('tests/fixtures/' + name) as fp:
         return json.load(fp)
+
 
 class TestDeGiroDict(TestCase):
     def test_free_space(self):
@@ -29,3 +31,25 @@ class TestDeGiroDict(TestCase):
         d = DeGiroDict(data)
         ids = [positionrow['id'] for positionrow in d['portfolio']['portfolio']['conttype']['positionrow']]
         self.assertEquals(ids, ['4998723', '4999807'])
+
+
+
+class InterfaceTest(TestCase):
+    def test_get_portfolio_joins_funds_and_portfolio(self):
+        session = Mock()
+        session.get_portfolio.return_value = [{
+            'id': '4998723',
+            'size': 0.821
+        }]
+        session.get_funds.return_value = [{
+            'id': '4998723',
+            'isin': 'LI0837977205',
+            'name': 'ABERDEEN GL-INDIA EQTY-X-2A',
+        }, {
+            'id': '5003027',
+        }]
+
+        i = Interface(session)
+        portfolio = i.get_portfolio()
+        self.assertEquals(len(portfolio), 1)
+        self.assertEquals(portfolio[0].fund.isin, 'LI0837977205')
